fb61b0c2363e3926e11ea10e679e32c4209a9623
Aleksandr Samartsev <TheMegaSide@outlook.com>
2020-10-18 12:35:55 +0400

    Entities created

---
+++ OnlineFurniture/Domain/Model/Category.cs
@@ -0,0 +1,17 @@
+﻿
+
+using OnlineFurniture.Domain.Model.Common;
+
+namespace OnlineFurniture.Domain.Model
+{
+    /// <summary>
+    /// Сущность категории товара
+    /// </summary>
+    public class Category : Entity
+    {
+        /// <summary>
+        /// Наименование категории
+        /// </summary>
+        public string CategoryName { get; set; }
+    }
+}
---
+++ OnlineFurniture/Domain/Model/User.cs
@@ -0,0 +1,40 @@
+﻿using OnlineFurniture.Domain.Model.Common;
+
+namespace OnlineFurniture.Domain.Model
+{
+    /// <summary>
+    /// Сущность-пользователь
+    /// </summary>
+    public class User : Entity
+    {
+        /// <summary>
+        /// Роль пользователя
+        /// </summary>
+        public string Role { get; set; }
+
+        /// <summary>
+        /// Фамилия пользователя
+        /// </summary>
+        public string Surname { get; set; }
+
+        /// <summary>
+        /// Имя пользователя
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Отчество пользователя
+        /// </summary>
+        public string Patronymic { set; get; }
+
+        /// <summary>
+        /// Адрес пользователя
+        /// </summary>
+        public string Address { get; set; }
+
+        /// <summary>
+        /// Телефонный номер
+        /// </summary>
+        public string Phone { get; set; }
+    }
+}
