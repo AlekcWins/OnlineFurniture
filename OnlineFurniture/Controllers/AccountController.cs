244f2b53e973b0bca9ad4e0e043ec1b25f372c4c
AlekcWins <aleksand2000@mail.ru>
2020-11-09 13:17:18 +0500

    add user components

---
+++ OnlineFurniture/Controllers/AccountController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Identity;
+using OnlineFurniture.Models;
+
+namespace OnlineFurniture.Controllers
+{
+    [Route("api/[controller]"), ApiController]
+    public class AccountController : Controller
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly SignInManager<ApplicationUser> _signInManager;
+
+        public AccountController(UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+
+        [HttpPost]
+        [Route("Register")]
+        //POST : /api/ApplicationUser/Register
+
+        //TODO ApplicationUserModel ->  RegisterViewModel
+        public async Task<Object> PostApplicationUser(ApplicationUserModel model)
+        {
+            var applicationUser = new ApplicationUser()
+            {
+                UserName = model.UserName,
+                Email = model.Email,
+            };
+
+            try
+            {
+                var result = await _userManager.CreateAsync(applicationUser, model.Password);
+                return Ok(result);
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+        }
+    }
+}
---
+++ OnlineFurniture/Models/ApplicationUserModel.cs
@@ -0,0 +1,9 @@
+﻿namespace OnlineFurniture.Models
+{
+    public class ApplicationUserModel
+    {
+        public string UserName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
