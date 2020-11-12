0724dedf08fa9aef41de95986d9db474d9fcdb46
AlekcWins <aleksand2000@mail.ru>
2020-11-12 18:50:41 +0500

    delete waste files

--- OnlineFurniture/ClientApp/src/app/app.module.ts
+++ OnlineFurniture/ClientApp/src/app/app.module.ts
@@ -1,29 +1,23 @@
 import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
-import {HttpClientModule, HTTP_INTERCEPTORS} from '@angular/common/http';
+import {HTTP_INTERCEPTORS, HttpClientModule} from '@angular/common/http';
 import {RouterModule} from '@angular/router';
 import {AppComponent} from './app.component';
 import {NavMenuComponent} from './nav-menu/nav-menu.component';
-import {HomeComponent} from './home/home.component';
-import {CounterComponent} from './counter/counter.component';
-import {FetchDataComponent} from './fetch-data/fetch-data.component';
 import {ApiAuthorizationModule} from 'src/api-authorization/api-authorization.module';
 import {AuthorizeGuard} from 'src/api-authorization/authorize.guard';
 import {AuthorizeInterceptor} from 'src/api-authorization/authorize.interceptor';
 import {UserComponent} from './user/user.component';
 import {RegistrationComponent} from './user/registration/registration.component';
-import {UserService} from './shared/user.service';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {ToastrModule} from 'ngx-toastr';
+import {UserService} from './shared/user.service';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavMenuComponent,
-    HomeComponent,
-    CounterComponent,
-    FetchDataComponent,
     UserComponent,
     RegistrationComponent,
   ],
@@ -41,8 +35,6 @@ import {ToastrModule} from 'ngx-toastr';
     // TODO move the route to a separate file
     RouterModule.forRoot([
       {path: '', redirectTo: '/user/registration', pathMatch: 'full'},
-      {path: 'counter', component: CounterComponent},
-      {path: 'fetch-data', component: FetchDataComponent, canActivate: [AuthorizeGuard]},
       {
         path: 'user', component: UserComponent,
         children: [
