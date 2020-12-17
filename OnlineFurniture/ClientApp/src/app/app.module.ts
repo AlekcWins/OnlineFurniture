13a46f55ebd37291bde630217b7a441d4c1d0814
AlekcWins <aleksand2000@mail.ru>
2020-12-17 18:24:16 +0500

    init front

--- OnlineFurniture/ClientApp/src/app/app.module.ts
+++ OnlineFurniture/ClientApp/src/app/app.module.ts
@@ -1,5 +1,7 @@
 import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
+
+import {AppRoutingModule} from './app-routing.module';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {HTTP_INTERCEPTORS, HttpClientModule} from '@angular/common/http';
 import {RouterModule} from '@angular/router';
@@ -8,11 +10,28 @@ import {NavMenuComponent} from './nav-menu/nav-menu.component';
 import {ApiAuthorizationModule} from 'src/api-authorization/api-authorization.module';
 import {AuthorizeGuard} from 'src/api-authorization/authorize.guard';
 import {AuthorizeInterceptor} from 'src/api-authorization/authorize.interceptor';
+
+import {BrowserAnimationsModule, NoopAnimationsModule} from '@angular/platform-browser/animations';
+import {NgxSpinnerModule} from 'ngx-spinner';
+
+import {ToastrModule} from 'ngx-toastr';
+
+
+import {UserService} from './shared/user.service';
+
 import {UserComponent} from './user/user.component';
 import {RegistrationComponent} from './user/registration/registration.component';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {ToastrModule} from 'ngx-toastr';
-import {UserService} from './shared/user.service';
+
+
+import {HeaderComponent} from './components/header/header.component';
+import {FooterComponent} from './components/footer/footer.component';
+import {HomeComponent} from './components/home/home.component';
+import {LoginComponent} from './components/login/login.component';
+import {RegisterComponent} from './components/register/register.component';
+import {CartComponent} from './components/cart/cart.component';
+import {CheckoutComponent} from './components/checkout/checkout.component';
+import {ProductComponent} from './components/product/product.component';
+import {ThankyouComponent} from './components/thankyou/thankyou.component';
 
 @NgModule({
   declarations: [
@@ -20,28 +39,29 @@ import {UserService} from './shared/user.service';
     NavMenuComponent,
     UserComponent,
     RegistrationComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    LoginComponent,
+    RegisterComponent,
+    CartComponent,
+    CheckoutComponent,
+    ProductComponent,
+    ThankyouComponent
   ],
   imports: [
     BrowserModule.withServerTransition({appId: 'ng-cli-universal'}),
-    HttpClientModule,
-    FormsModule,
-    HttpClientModule,
-    ApiAuthorizationModule,
-    ReactiveFormsModule,
     BrowserAnimationsModule,
+    NoopAnimationsModule,
+    AppRoutingModule,
+    HttpClientModule,
+    NgxSpinnerModule,
+    ReactiveFormsModule,
+    FormsModule,
+    ApiAuthorizationModule,
     ToastrModule.forRoot({
       progressBar: true
     }),
-    // TODO move the route to a separate file
-    RouterModule.forRoot([
-      {path: '', redirectTo: '/user/registration', pathMatch: 'full'},
-      {
-        path: 'user', component: UserComponent,
-        children: [
-          {path: 'registration', component: RegistrationComponent}
-        ]
-      },
-    ])
   ],
   providers: [
     UserService,
